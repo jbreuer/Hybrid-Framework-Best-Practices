f7789e8363e087328b99435b011a51dfc49c200d
Jeavon Leopold <jeavon@crumpled-dog.com>
2013-09-21 09:34:25 +0100

    Fixing broken VS reference to Slimmage and adding un minified version for debug

--- development/Umbraco.Site/scripts/slimmage.js
+++ development/Umbraco.Site/scripts/slimmage.js
@@ -1,8 +1,165 @@
 ﻿/* Slimmage 0.2.1, use with ImageResizer. MIT/Apache2 dual licensed by Imazen */
 
-(function(b){function p(){b.slimmage.verbose&&(b.console&&b.console.log)&&b.console.log.apply(b.console,arguments)}var a=window.slimmage||{};window.slimmage=a;void 0===a.verbose&&(a.verbose=!0);void 0===a.tryWebP&&(a.tryWebP=!1);a.h=function(){if(a.tryWebP&&!a.e){a.e=!0;var e=new Image;e.onload=e.onerror=function(){a.d=2==e.height};e.src="data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA"}};a.j=function(a){var c="undefined"!=typeof window.getComputedStyle&&
-window.getComputedStyle(a,null).getPropertyValue("max-width");!c&&a.currentStyle&&(c=a.currentStyle["max-width".replace(/([a-z])\-([a-z])/,function(a,e,c){return e+c.toUpperCase()})]||a.currentStyle["max-width"]);return c};a.i=function(e){var c=a.j(e);if("px"==c.slice(-2))return parseFloat(c.slice(0,-2));var d=document.createElement("div");d.style.overflow=d.style.visibility="hidden";e.parentNode.appendChild(d);d.style.width=c;c=d.offsetWidth;e.parentNode.removeChild(d);return c};a.c=function(a){for(var c=
-[],d=a.length>>>0;d--;)c[d]=a[d];return c};a.g=function(e,c,d){var b=window.devicePixelRatio||1,n=1.49<b?90:80;a.d&&(n=1.49<b?65:78);d=Math.min(2048,d*b);d=d-d%160+160;b=e.getAttribute("data-pixel-width")|0;d>b&&(c=c.replace(/width=\d+/i,"width="+d).replace(/quality=[0-9]+/i,"quality="+n),a.d&&(c=c.replace(/format=[a-z]+/i,"format=webp")),e.src=c,e.setAttribute("data-pixel-width",d),p("Slimming: updating "+c))};a.f=function(b,c){a.g(b,c,a.i(b))};a.b=function(e){0<a.a&&b.clearTimeout(a.a);a.a=0;if(e&&
-0<e)a.a=b.setTimeout(a.b,e);else{e=(new Date).getTime();for(var c=0,d=a.c(b.document.getElementsByTagName("noscript")),k=0,n=d.length;k<n;k++){var f=d[k];if(null!==f.getAttribute("data-slimmage")){var m=b.document.createElement("div"),l=f.textContent||f.innerHTML;if(l&&0!=l.replace(/[\s\t\r\n]+/,"").length)m.innerHTML=l.replace(/\s+src\s*=\s*(['"])/i," data-src=$1").replace(/&lt;/g,"<").replace(/&gt;/g,">").replace(/&amp;/g,"&");else{for(var l=new Image,h=0;h<f.attributes.length;h++){var g=f.attributes[h];
-g&&(g.specified&&0==g.name.indexOf("data-img-"))&&l.setAttribute(g.name.slice(9-g.name.length),g.value)}m.appendChild(l)}m=m.getElementsByTagName("img");l=0;for(h=m.length;l<h;l++)g=m[l],null!==g.src&&0<g.src.length&&(g.setAttribute("data-src",g.src),g.src=""),g.setAttribute("data-slimmage",!0),f.parentNode.insertBefore(g,f),c++;f.parentNode.removeChild(f)}}d=0;f=a.c(b.document.getElementsByTagName("img"));k=0;for(n=f.length;k<n;k++)null!==f[k].getAttribute("data-slimmage")&&(m=f[k].getAttribute("data-src")||
-f[k].src,a.f(f[k],m),d++);p("Slimmage: restored "+c+" images from noscript tags; sizing "+d+" images. "+((new Date).getTime()-e)+"ms")}};var h=a.b;b.addEventListener?(b.addEventListener("resize",function(){h(500)},!1),b.addEventListener("DOMContentLoaded",function(){h();b.removeEventListener("load",h,!1)},!1),b.addEventListener("load",h,!1)):b.attachEvent&&b.attachEvent("onload",h);a.h()})(this);
+(function (w) { //w==window
+    // Enable strict mode
+    "use strict";
+
+    var s =  window.slimmage || {};
+    /** @expose **/
+    window.slimmage = s;
+    if (s.verbose === undefined) /** @expose **/ s.verbose = true;
+    if (s.tryWebP === undefined) /** @expose **/ s.tryWebP = false;
+
+    var log = function(){ if (w.slimmage.verbose && w.console && w.console.log) w.console.log.apply(w.console,arguments);};
+    s.beginWebPTest = function(){
+        if (!s.tryWebP || s._testingWebP) return;
+        s._testingWebP = true;
+
+        var WebP=new Image();
+        WebP.onload=WebP.onerror=function(){
+            s.webp = (WebP.height==2);
+        };
+        WebP.src='data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+    };
+   
+    s.getCssValue = function(target, hyphenProp){
+      var val = typeof(window.getComputedStyle) != "undefined" && window.getComputedStyle(target, null).getPropertyValue(hyphenProp);
+      if (!val && target.currentStyle){
+        val = target.currentStyle[hyphenProp.replace(/([a-z])\-([a-z])/, function(a,b,c){ return b + c.toUpperCase();})] || target.currentStyle[hyphenProp];
+      }
+      return val;
+    };
+
+    s.getCssPixels = function(target, hyphenProp){
+      var val = s.getCssValue(target,hyphenProp);
+
+      //We can return pixels directly, but not other units
+      if (val.slice(-2) == "px") return parseFloat(val.slice(0,-2));
+
+      //Create a temporary sibling div to resolve units into pixels.
+      var temp = document.createElement("div");
+      temp.style.overflow = temp.style.visibility = "hidden"; 
+      target.parentNode.appendChild(temp);  
+      temp.style.width = val;
+      var pixels = temp.offsetWidth;
+      target.parentNode.removeChild(temp);
+      return pixels;
+    };
+
+    s.nodesToArray = function (nodeList) {
+        var array = [];
+        // iterate backwards ensuring that length is an UInt32
+        for (var i = nodeList.length >>> 0; i--;) {
+            array[i] = nodeList[i];
+        }
+        return array;
+    };
+    //Expects virtual, not device pixel width
+    s.adjustImageSrcWithWidth = function (img, originalSrc, width) {
+        var dpr = window.devicePixelRatio || 1;
+        var trueWidth = width * dpr;
+
+        var quality = (dpr > 1.49) ? 90 : 80;
+
+        if (s.webp) quality = dpr > 1.49 ? 65 : 78;
+
+        var maxwidth = Math.min(2048, trueWidth); //Limit size to 2048.
+
+        //Minimize variants for caching improvements; round up to nearest multiple of 160
+        maxwidth = maxwidth - (maxwidth % 160) + 160; //Will limit to 13 variations
+
+        var oldpixels = img.getAttribute("data-pixel-width") | 0;
+
+        if (maxwidth > oldpixels) {
+            //Never request a smaller image once the larger one has already started loading
+            var newSrc = originalSrc.replace(/width=\d+/i, "width=" + maxwidth).replace(/quality=[0-9]+/i,"quality=" + quality);
+            if (s.webp) newSrc = newSrc.replace(/format=[a-z]+/i,"format=webp");
+            img.src =  newSrc; 
+            img.setAttribute("data-pixel-width", maxwidth);
+            log("Slimming: updating " + newSrc)
+        }
+    };
+    s.adjustImageSrc = function (img, originalSrc) {
+        s.adjustImageSrcWithWidth(img, originalSrc, s.getCssPixels(img, "max-width"));
+    };
+
+    s.checkResponsiveImages = function (delay) {
+        if (s.timeoutid > 0) w.clearTimeout(s.timeoutid);
+        s.timeoutid = 0;
+        if (delay && delay > 0) {
+            s.timeoutid = w.setTimeout(s.checkResponsiveImages, delay);
+            return;
+        }
+        var stopwatch = new Date().getTime();
+
+        var newImages = 0;
+        //1. Copy images out of noscript tags, but hide 'src' attribute as data-src
+        var n = s.nodesToArray(w.document.getElementsByTagName("noscript"));
+        for (var i = 0, il = n.length; i < il; i++) {
+            var ns = n[i];
+            if (ns.getAttribute("data-slimmage") !== null) {
+                
+                var div = w.document.createElement('div');
+                var contents = (ns.textContent || ns.innerHTML);
+                if (!contents || contents.replace(/[\s\t\r\n]+/,"").length == 0){
+                    //IE doesn't let us touch noscript, so we have to use attributes.
+                    var img = new Image();
+                    for (var ai = 0; ai < ns.attributes.length; ai++) {
+                        var a = ns.attributes[ai];
+                        if (a && a.specified && a.name.indexOf("data-img-") == 0){
+                            img.setAttribute(a.name.slice(9 - a.name.length),a.value);
+                        }
+                    }
+                    div.appendChild(img);
+                }else{
+                    //noscript isn't part of DOM, so we have to recreate it, unescaping html, src->data-src 
+                    div.innerHTML = contents.replace(/\s+src\s*=\s*(['"])/i, " data-src=$1").
+                        replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&amp;/g, '&');
+                }
+                //Clear source values before we add it back to the dom, ensure data-slimmage is set.
+                var childImages = div.getElementsByTagName("img");
+                for (var j = 0, jl = childImages.length; j < jl; j++) {
+                    var ci = childImages[j];
+                    if (ci.src !== null && ci.src.length > 0) {
+                        ci.setAttribute("data-src", ci.src);
+                        ci.src = "";
+                    }
+                    ci.setAttribute("data-slimmage", true);
+                    ns.parentNode.insertBefore(ci, ns);
+                    newImages++;
+                }
+                //2. Remove old noscript tags
+                ns.parentNode.removeChild(ns);
+            }
+        }
+
+        //3. Find images with data-slimmage and run adjustImageSrc.
+        var totalImages = 0;
+        var images = s.nodesToArray(w.document.getElementsByTagName("img"));
+        for (var i = 0, il = images.length; i < il; i++) {
+            if (images[i].getAttribute("data-slimmage") !== null) {
+                var originalSrc = images[i].getAttribute("data-src") || images[i].src;
+                s.adjustImageSrc(images[i], originalSrc);
+                totalImages++;
+            }
+        }
+
+        log("Slimmage: restored " + newImages + " images from noscript tags; sizing " + totalImages + " images. " + (new Date().getTime() - stopwatch) + "ms");
+    };
+
+    var h = s.checkResponsiveImages;
+    // Run on resize and domready (w.load as a fallback)
+    if (w.addEventListener) {
+        w.addEventListener("resize", function () { h(500); }, false);
+        w.addEventListener("DOMContentLoaded", function () {
+            h();
+            // Run once only
+            w.removeEventListener("load", h, false);
+        }, false);
+        w.addEventListener("load", h, false);
+    } else if (w.attachEvent) {
+        w.attachEvent("onload", h);
+    }
+    //test for webp support
+    s.beginWebPTest();
+}(this));
