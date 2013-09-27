2ed177f0bbbf389c9fc1f4608ebf541cd842a865
Jeavon Leopold <jeavon@crumpled-dog.com>
2013-09-27 18:59:07 +0100

    Add JS Namespace to functions.js to ensure no conflicts

--- development/Umbraco.Site/scripts/functions.js
+++ development/Umbraco.Site/scripts/functions.js
@@ -1,25 +1,32 @@
-﻿$(document).ready(function () {
-    initValidations();
-});
+﻿var UmbracoProject = UmbracoProject || {};
 
-//Form validations
-function initValidations() {
-    if ($("#contactForm").length > 0) {
-        $("#contactForm").validate({
-            rules: {
-                Email: {
-                    required: true,
-                    email: true
-                }
-            },
-            messages: {
-                Name: $("input[name='Name']").data("val-required"),
-                Email: {
-                    required: $("input[name='Email']").data("val-required"),
-                    email: $("input[name='Email']").data("val-invalid")
+UmbracoProject.Validation = UmbracoProject.Validation || (function () {
+    //Form validations
+    function initValidations() {
+        if ($("#contactForm").length > 0) {
+            $("#contactForm").validate({
+                rules: {
+                    Email: {
+                        required: true,
+                        email: true
+                    }
                 },
-                Comment: $("textarea[name='Comment']").data("val-required")
-            }
-        });
+                messages: {
+                    Name: $("input[name='Name']").data("val-required"),
+                    Email: {
+                        required: $("input[name='Email']").data("val-required"),
+                        email: $("input[name='Email']").data("val-invalid")
+                    },
+                    Comment: $("textarea[name='Comment']").data("val-required")
+                }
+            });
+        }
     }
-}
+    return {
+        initValidations: initValidations
+    };
+}());
+
+$(document).ready(function () {
+    window.UmbracoProject.Validation.initValidations();
+});
